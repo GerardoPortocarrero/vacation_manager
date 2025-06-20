eb2fb1e681b4ce8e3a97c97bdd01f4eaea534431
GerardoPortocarrero <gersrpb@gmail.com>
2025-06-20 09:57:49 -0500

    20/06/2025 - prueba de envio de correos

--- show_html.py
+++ show_html.py
@@ -1,8 +1,9 @@
 import os
 import webbrowser
 
-def main(file_name):
+def main(project_address, file_name):
+    file_address = os.path.join(project_address, file_name)
     # Ruta del archivo HTML
-    file_path = os.path.abspath(file_name)
+    file_path = os.path.abspath(file_address)
     # Abrir en navegador predeterminado
     webbrowser.open(f"file://{file_path}")
